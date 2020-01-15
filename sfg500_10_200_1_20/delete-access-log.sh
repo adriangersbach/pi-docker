babf0702b306225585806ac4cfc6dbd996e5bf56
adriangersbach <42101979+adriangersbach@users.noreply.github.com>
2020-01-15 16:53:29 +0100

    Create delete-access-log.sh

---
+++ sfg500_10_200_1_20/delete-access-log.sh
@@ -0,0 +1,10 @@
+sudo rm /var/log/nginx/sfg500/11/access.log
+sudo rm /var/log/nginx/sfg500/12/access.log
+sudo rm /var/log/nginx/sfg500/13/access.log
+sudo rm /var/log/nginx/sfg500/14/access.log
+sudo rm /var/log/nginx/sfg500/15/access.log
+sudo rm /var/log/nginx/sfg500/16/access.log
+sudo rm /var/log/nginx/sfg500/17/access.log
+sudo rm /var/log/nginx/sfg500/18/access.log
+sudo rm /var/log/nginx/sfg500/19/access.log
+sudo rm /var/log/nginx/sfg500/20/access.log
